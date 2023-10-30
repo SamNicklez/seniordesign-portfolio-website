615bf27d0e82fa436c75b3d5674b09237f010b89
SamNicklez <snicklaus@uiowa.edu>
2023-10-30 14:16:01 -0500

    added user comments for lab memebers

--- src/views/Saml.vue
+++ src/views/Saml.vue
@@ -1,10 +1,26 @@
 <script>
+// JAVASCRIPT STUFF GOES HERE, examples below
+export default {
+    data() {
+        return {
+            globalVariable: true, //Access the variables in methods by calling this.globalVariable
+        }
+    },
+    methods: {
+        doStuff(){
+            console.log(this.globalVariable) //prints a global variable
+        }
+    },
+}
 </script>
 
 <template>
-Hello World
+    <!-- HTML STUFF GOES HERE -->
+    <div>
+        Hello World
+    </div>
 </template>
 
 <style>
-
+/* CSS STUFF GOES HERE */
 </style>
--- src/views/Samn.vue
+++ src/views/Samn.vue
@@ -1,18 +1,26 @@
 <script>
-import { useCookies } from "vue3-cookies";
-
-export default{
-  setup() {
-    const { cookies } = useCookies();
-    return { cookies };
-  },
+// JAVASCRIPT STUFF GOES HERE, examples below
+export default {
+    data() {
+        return {
+            globalVariable: true, //Access the variables in methods by calling this.globalVariable
+        }
+    },
+    methods: {
+        doStuff(){
+            console.log(this.globalVariable) //prints a global variable
+        }
+    },
 }
 </script>
 
 <template>
-Hello World
+    <!-- HTML STUFF GOES HERE -->
+    <div>
+        Hello World
+    </div>
 </template>
 
 <style>
-
+/* CSS STUFF GOES HERE */
 </style>
