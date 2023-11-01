f4d67d2e69cbd443ab903ff6962d5337f7e4e68c
SamNicklez <snicklaus@uiowa.edu>
2023-11-01 12:03:16 -0500

    Updated logos and packages

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,48 @@
 <script>
 import { useCookies } from "vue3-cookies";
-
-export default{
-  setup() {
-    const { cookies } = useCookies();
-    return { cookies };
-  },
-  mounted() {
-    // let my_cookie_value = this.cookies.get("myCoookie")
-    // console.log(my_cookie_value)
-    // this.cookies.set("myCoookie", "abcdefg")
-  },
+import { db } from '@/main.js';
+import { collection, getDocs } from 'firebase/firestore';
+export default {
+    data() {
+        return {
+            items: [],
+        }
+    },
+    setup() {
+        const { cookies } = useCookies();
+        return { cookies };
+    },
+    mounted() {
+        // let my_cookie_value = this.cookies.get("myCoookie")
+        // console.log(my_cookie_value)
+        // this.cookies.set("myCoookie", "abcdefg")
+    },
+    async created(){
+        const querySnapshot = await getDocs(collection(db, 'User'));
+        this.items = querySnapshot.docs.map(doc => doc.data());
+    },
+    methods: {
+        doStuff() {
+            console.log(this.items)
+        }
+    },
 }
 </script>
 
 <template>
-<body>
-
-</body>
+    <body>
+        <v-btn @click="doStuff()">click me</v-btn>
+    </body>
 </template>
 
 <style>
-body, html {
+body,
+html {
     margin: 0;
     padding: 0;
     font-family: Arial, sans-serif;
-    overflow: hidden; /* To ensure no scrollbars appear */
+    overflow: hidden;
+    /* To ensure no scrollbars appear */
 }
 
 .center-container {
@@ -33,12 +50,14 @@ body, html {
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    height: 75vh; /* 100% of the viewport height */
-    width: 100vw;  /* 100% of the viewport width */
+    height: 75vh;
+    /* 100% of the viewport height */
+    width: 100vw;
+    /* 100% of the viewport width */
 }
 
 .button-container {
-    margin: 1vh 0; /* Using viewport height units for spacing */
+    margin: 1vh 0;
+    /* Using viewport height units for spacing */
 }
-
 </style>
